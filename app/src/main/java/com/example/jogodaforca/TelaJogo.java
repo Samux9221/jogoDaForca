31cc6222a9160fadacab22ca9bb6d831a7536724
aluno <samuelfernandessilva012@gmail.com>
2026-05-04 11:30:57 -0300

    04/05

--- app/src/main/java/com/example/jogodaforca/TelaJogo.java
+++ app/src/main/java/com/example/jogodaforca/TelaJogo.java
@@ -1,6 +1,9 @@
 package com.example.jogodaforca;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +11,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class TelaJogo extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class TelaJogo extends AppCompatActivity implements View.OnClickListener {
+
+    private ImageView imagem;
+    private ArrayList<Integer> listaImagens;
+    private int indiceListaImagens;
+    private Button b1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +30,37 @@ public class TelaJogo extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        //ligando imagem de forca do primeiro layout e da segunda tela
+        imagem = findViewById(R.id.imageView);
+        b1 = findViewById(R.id.id1);
+
+        indiceListaImagens = 0;
+        listaImagens = new ArrayList<Integer>(); //instanciando o arrayList, é necessário
+
+        //alimentando as imagens na lista de imagens, em ordem logica e sequancial
+        listaImagens.add(R.drawable.forca_1_9);
+        listaImagens.add(R.drawable.forca_2_9);
+        listaImagens.add(R.drawable.forca_3_9);
+        listaImagens.add(R.drawable.forca_4_9);
+        listaImagens.add(R.drawable.forca_5_9);
+        listaImagens.add(R.drawable.forca_6_9);
+        listaImagens.add(R.drawable.forca_7_9);
+        listaImagens.add(R.drawable.forca_8_9);
+        listaImagens.add(R.drawable.forca_9_9);
+        listaImagens.add(R.drawable.forca_10_9);
+
+        b1.setOnClickListener(this);
+    }
+
+    public void atualizaForca(){
+        imagem.setImageResource(listaImagens.get(indiceListaImagens));
+
+        indiceListaImagens++;
+    }
+
+    @Override
+    public void onClick(View v) {
+        atualizaForca();
     }
 }
